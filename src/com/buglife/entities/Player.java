94a18078c2862617ed7f9d874eaa32ba6dcc8617
SaiV-0518 <ssvivek0518@gmail.com>
2025-11-21 00:06:39 +0530

    speed adjust player

--- src/com/buglife/entities/Player.java
+++ src/com/buglife/entities/Player.java
@@ -25,7 +25,7 @@ public class Player {
     private double x, y;
     private int width, height;
     private double currentSpeed; // How fast we are moving RIGHT NOW
-    private final double NORMAL_SPEED = 1.6; // The default speed
+    private final double NORMAL_SPEED = 2.3; // The default speed
     private final double SLOW_SPEED = 0.5; // The speed when stuck!
     private int hunger = 100;
     private final int MAX_HUNGER = 100;
